5ba452b2a1c3524cbabf91da36681513e30b7755
joaolira-dev <joaovictorlira06@hotmail.com>
2025-02-20 21:40:01 -0300

    Created base of project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # pet-devshop
+
+### Pré requisitos globais:
+"npm install -g nodemon typescript ts-node"
+
+### Instalacao
+"npm install"
+
+### Para rodar o projeto:
+"npm run start-dev"
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+export const home = (req: Request, res: Response) => {
+   res.send("home no controller!")
+   // page
+}
+
+export const dogs = (req: Request, res: Response) => {
+
+   // page
+}
+
+export const cats = (req: Request, res: Response) => {
+
+   // page
+}
+
+export const fishes = (req: Request, res: Response) => {
+
+   // page
+}
+
+
